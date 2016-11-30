c12f239625edd32edafb94296007c4b5312af3d7
Grzegorz Brodowski <Grzegorz Brodowski@E5430>
2016-11-30 22:01:56 +0100

    Add project files.

---
+++ gbwebapp/checkbox_manipulacje.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="checkbox_manipulacje.aspx.cs" Inherits="gbwebapp.checkbox_manipulacje" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: larger;
+        }
+        .auto-style2 {
+            text-align: center;
+        }
+        .auto-style3 {
+            font-size: x-large;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="auto-style2">
+    
+        <span class="auto-style3">Testowa grupa checkboxów:<br />
+        </span>
+    
+        <span class="auto-style1">
+        <br />
+        </span>
+        <asp:CheckBox ID="chbx1" runat="server" OnCheckedChanged="chbx1_Checked" Text="CheckBox 1" />
+        <br />
+        <asp:CheckBox ID="chbx2" runat="server" OnCheckedChanged="chbx2_CheckedChanged" Text="CheckBox 2" />
+        <br />
+        <asp:CheckBox ID="chbx3" runat="server" OnCheckedChanged="chbx3_CheckedChanged" Text="CheckBox 3" />
+        <br />
+    
+    </div>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </p>
+        <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        <p>
+            <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ gbwebapp/checkbox_manipulacje.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.DirectoryServices;
+using MySql.Data.MySqlClient;
+
+namespace gbwebapp
+{
+    public partial class checkbox_manipulacje : System.Web.UI.Page
+    {
+        MySqlConnection connection;
+        string ConnectionString = gbwebapp.Properties.Settings.Default.ConnectionString;
+        string checkbox1_insert = "INSERT INTO web_test_skasowac.stany_checkboxow(stany_checkboxa, nazwa_checkboxa, znacznik_czasowy, stany_checkboxa_bool, uwagi) VALUES ('zaznaczony','CheckBox 1', " + DateTime.Now + ", 1, 'udało się wstawić wartość')";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        //protected void chbx1_CheckedChanged(object sender, EventArgs e)
+        //{
+            
+        //    MySqlCommand cbx1_insert = new MySqlCommand();
+        //    cbx1_insert.CommandType = System.Data.CommandType.Text;
+        //    cbx1_insert.CommandText = "INSERT INTO web_test_skasowac.stany_checkboxow(stany_checkboxa, nazwa_checkboxa, znacznik_czasowy, stany_checkboxa_bool, uwagi) VALUES ('zaznaczony','CheckBox 1', " + DateTime.Now + ", 1, 'udało się wstawić wartość')";
+        //    cbx1_insert.Connection = connection;
+
+        //    connection.Open();
+        //    cbx1_insert.ExecuteNonQuery();
+        //    connection.Close();
+        //}
+        protected void chbx1_Checked(object sender, EventArgs e)
+        {
+            connection = new MySqlConnection(ConnectionString);
+            MySqlCommand cbx1_insert = new MySqlCommand();
+            cbx1_insert.CommandType = System.Data.CommandType.Text;
+            cbx1_insert.CommandText = "INSERT INTO web_test_skasowac.stany_checkboxow(stany_checkboxa, nazwa_checkboxa, znacznik_czasowy, stany_checkboxa_bool, uwagi) VALUES ('zaznaczony','CheckBox 1', " + DateTime.Now + ", 1, 'udało się wstawić wartość')";
+            cbx1_insert.Connection = connection;
+
+            connection.Open();
+            cbx1_insert.ExecuteNonQuery();
+            connection.Close();
+            Label1.Text = "G1"; 
+        }
+
+        protected void chbx2_CheckedChanged(object sender, EventArgs e)
+        {
+            Label2.Text = "G2";
+        }
+
+        protected void chbx3_CheckedChanged(object sender, EventArgs e)
+        {
+            Label3.Text = "G3";
+        }
+    }
+}
