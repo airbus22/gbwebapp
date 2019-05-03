43f52ea4063024ae7e083ba0a94acbd844a2f75c
GB - home <grzegorz.brodowski@outlook.com>
2019-05-04 00:07:03 +0200

    wyłączenie obsługi z bazą danych

--- gbwebapp/bd_rej_KonfSemOtwarte.aspx.cs
+++ gbwebapp/bd_rej_KonfSemOtwarte.aspx.cs
@@ -12,8 +12,8 @@ namespace gbwebapp
 {
     public partial class bd_rej_SemKonfOtwarte : System.Web.UI.Page
     {
-        MySqlConnection connection, pobierzMAXvalue;
-        string ConnectionString = gbwebapp.Properties.Settings.Default.ConnectionString;
+        //MySqlConnection connection, pobierzMAXvalue;
+        //string ConnectionString = gbwebapp.Properties.Settings.Default.ConnectionString;
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -27,7 +27,7 @@ namespace gbwebapp
 
         protected void ks_przeslij_btn_Click(object sender, EventArgs e)
         {
-            string nadawca = "grzegorz.brodowski@ksap.gov.pl";
+            string nadawca = "powiadomienia@ksap.gov.pl";
             string odbiorca = email_tbx.Text;
             string temat = "Potwierdzenie uczestnictwa w wydarzeniu w KSAP";
             string tekst_wiadomosci = "Potwierdzamy rejestrację na wydarzenie w KSAP.";
@@ -50,30 +50,29 @@ namespace gbwebapp
                 klientEmail.UseDefaultCredentials = false;
                 klientEmail.Credentials = SMTP_dane_uzytkownika;
                 klientEmail.EnableSsl = true;
-                //klientEmail.Timeout = 500000;
                 klientEmail.Send(wiadonmosc);
                 statusWiadomosci_lbl.ForeColor = System.Drawing.Color.Green;
                 statusWiadomosci_lbl.Text = "Zgłoszenie wysłane";
                 statusWiadomosci_lbl.Visible = true;
 
-                // tu DB
-                pobierzMAXvalue = new MySqlConnection(ConnectionString);
-                MySqlCommand zapytanieMAXvalue = new MySqlCommand("SELECT MAX(id_zgloszenia) FROM web_test_skasowac.bd_rej_semkonfotwart", pobierzMAXvalue);
-                zapytanieMAXvalue.Connection.Open();
-                string MAXval_ = zapytanieMAXvalue.ExecuteScalar().ToString();
-                if (MAXval_ == "")
-                    MAXval_ = "0";
+                //// tu DB
+                //pobierzMAXvalue = new MySqlConnection(ConnectionString);
+                //MySqlCommand zapytanieMAXvalue = new MySqlCommand("SELECT MAX(id_zgloszenia) FROM web_test_skasowac.bd_rej_semkonfotwart", pobierzMAXvalue);
+                //zapytanieMAXvalue.Connection.Open();
+                //string MAXval_ = zapytanieMAXvalue.ExecuteScalar().ToString();
+                //if (MAXval_ == "")
+                //    MAXval_ = "0";
 
-                int MAXval = UInt16.Parse(MAXval_) + 1;    //obliczenie wartości id_zgloszenia dla kolejnego INSERTa
+                //int MAXval = UInt16.Parse(MAXval_) + 1;    //obliczenie wartości id_zgloszenia dla kolejnego INSERTa
 
-                connection = new MySqlConnection(ConnectionString);
-                MySqlCommand przeslij_btn_insert = new MySqlCommand();
-                przeslij_btn_insert.CommandType = System.Data.CommandType.Text;
-                przeslij_btn_insert.CommandText = "INSERT INTO web_test_skasowac.bd_rej_semkonfotwart(id_zgloszenia,plec,imie,drugieImie,nazwisko,email,daneOsobowe_zgoda) VALUES (" + MAXval + "," + "'" + plec_ddl.SelectedValue.ToString() + "'" + "," + "'" + imie_tbx.Text + "'" + "," + "'" + drugieImie_tbx.Text + "'" + "," + "'" + nazwisko_tbx.Text + "'" + "," + "'" + email_tbx.Text + "'" + "," + "'" + daneOsobowe_zgoda_ddl.SelectedValue.ToString() + "'" + ")";
-                przeslij_btn_insert.Connection = connection;
-                connection.Open();
-                przeslij_btn_insert.ExecuteNonQuery();
-                connection.Close();
+                //connection = new MySqlConnection(ConnectionString);
+                //MySqlCommand przeslij_btn_insert = new MySqlCommand();
+                //przeslij_btn_insert.CommandType = System.Data.CommandType.Text;
+                //przeslij_btn_insert.CommandText = "INSERT INTO web_test_skasowac.bd_rej_semkonfotwart(id_zgloszenia,plec,imie,drugieImie,nazwisko,email,daneOsobowe_zgoda) VALUES (" + MAXval + "," + "'" + plec_ddl.SelectedValue.ToString() + "'" + "," + "'" + imie_tbx.Text + "'" + "," + "'" + drugieImie_tbx.Text + "'" + "," + "'" + nazwisko_tbx.Text + "'" + "," + "'" + email_tbx.Text + "'" + "," + "'" + daneOsobowe_zgoda_ddl.SelectedValue.ToString() + "'" + ")";
+                //przeslij_btn_insert.Connection = connection;
+                //connection.Open();
+                //przeslij_btn_insert.ExecuteNonQuery();
+                //connection.Close();
             }
             catch (Exception ex)
             {
