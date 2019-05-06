e2cc49729c40f1ef8129912c93aa264445ed89a8
GB - praca <airbus22@tlen.pl>
2019-05-06 14:33:42 +0200

    code behind - zmiana hasła

--- gbwebapp/bd_rej_KonfSemOtwarte.aspx.cs
+++ gbwebapp/bd_rej_KonfSemOtwarte.aspx.cs
@@ -39,7 +39,7 @@ namespace gbwebapp
             string SMTPServer = "smtp.office365.com";
             int SMTPPort = 587;
             string uzytkownik = "powiadomienia@ksap.gov.pl";
-            string haslo = "Ksaper321";
+            string haslo = "KpYg%8Ce*4@!hxEi";
             MailMessage wiadonmosc = new MailMessage(nadawca, odbiorca, temat, tekst_wiadomosci);
             //SmtpClient klientEmail = new SmtpClient(SMTPServer);
             SmtpClient klientEmail = new SmtpClient(SMTPServer, SMTPPort);
