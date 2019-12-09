0ff63cceda0bd1b990888c6330bed527dbecfc08
GB - praca <airbus22@tlen.pl>
2019-12-09 15:59:48 +0100

    już lepiej

--- gbwebapp/bd_rej_KonfSemOtwarte.aspx.cs
+++ gbwebapp/bd_rej_KonfSemOtwarte.aspx.cs
@@ -30,12 +30,12 @@ namespace gbwebapp
             string nadawca = "powiadomienia@ksap.gov.pl";
             string odbiorca = email_tbx.Text;
             string temat = "Potwierdzenie uczestnictwa w wydarzeniu w KSAP";
-            string tekst_wiadomosci = "Potwierdzamy rejestrację na wydarzenie w KSAP." + "\n\n" + "Płeć: " + plec_ddl.ToString() + "\n" +
-                "Imię: " + imie_tbx.ToString() + "\n" +
-                "Drugie imię: " + drugieImie_tbx.ToString() + "\n" +
-                "Nazwisko: " + nazwisko_tbx.ToString() + "\n" +
-                "Aders e-mail: " + email_tbx.ToString() + "\n" +
-                "Zgoda na przetwarzanie danych osobowych: " + daneOsobowe_zgoda_ddl.ToString();
+            string tekst_wiadomosci = "Potwierdzamy rejestrację na wydarzenie w KSAP." + "\n\n" + "Płeć: " + plec_ddl.SelectedValue.ToString() + "\n" +
+                "Imię: " + imie_tbx.Text.ToString() + "\n" +
+                "Drugie imię: " + drugieImie_tbx.Text.ToString() + "\n" +
+                "Nazwisko: " + nazwisko_tbx.Text.ToString() + "\n" +
+                "Aders e-mail: " + email_tbx.Text.ToString() + "\n" +
+                "Zgoda na przetwarzanie danych osobowych: " + daneOsobowe_zgoda_ddl.SelectedValue.ToString();
             //string SMTPServer = "148.81.171.126";
             //string SMTPServer = "mail.ksap.gov.pl/gw/webacc";
             //string SMTPServer = "http://mail.ksap.gov.pl";
